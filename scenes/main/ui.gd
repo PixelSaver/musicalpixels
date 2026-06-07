32c950d786caa27a71a6b56835852c4bfaf8aa86
PixelSaver <stephen.k@proton.me>
2026-06-06 21:58:03 -0700

    Added disappearing ui so the visualizer can be enjoyed!

--- scenes/main/ui.gd
+++ scenes/main/ui.gd
@@ -2,6 +2,8 @@
 extends Control
 class_name UI
 @onready var option_button: OptionButton = $OptionButton
+var countdown = 4.0
+var countdown_max = 4.0
 
 func _ready() -> void:
 	#option_button.clear()
@@ -10,3 +12,13 @@ func _ready() -> void:
 	option_button.item_selected.connect(func(idx):
 		Global.switch_to_visualizer.emit(idx)
 	)
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		self.modulate.a = 1.0
+		countdown = countdown_max
+
+func _process(delta: float) -> void:
+	countdown -= delta
+	if countdown < 0:
+		self.modulate.a = clamp(lerp(modulate.a, 0., 0.1), 0.0, 1.0)
